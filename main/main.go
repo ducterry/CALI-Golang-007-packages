44b533e0a511e6177dda1e0a1a330add3de1c04f
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-22 08:34:36 +0700

    Update code 71

--- main/main.go
+++ main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
 /*
@@ -15,5 +17,19 @@ func main() {
 	number2 := 10.5
 	var maxNumber = math.Max(number1, number2)
 	fmt.Printf("Số lớn nhất giữa %.2f bà %.2f là %.2f", number1, number2, maxNumber)
+
+	// Calculate the square root of a number
+	fmt.Println(math.Sqrt(225))
+
+	// Printing the value of `𝜋`
+	fmt.Println(math.Pi)
+
+	// Epoch time in milliseconds
+	epoch := time.Now().Unix()
+	fmt.Println(epoch)
+
+	// Generating a random integer between 0 to 100
+	rand.Seed(epoch)
+	fmt.Println(rand.Intn(100))
 
 }

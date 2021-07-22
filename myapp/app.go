7b84650dea9113a9d1537a56e558e27164a381ad
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-22 12:34:01 +0700

    Update code 71

--- myapp/app.go
+++ myapp/app.go
@@ -1,11 +1,19 @@
 package main
 
+import (
+	"07-packages/numbers"
+	"07-packages/strings"
+	"07-packages/strings/greeting"
+	"fmt"
+	str "strings"
+)
+
 func main() {
-	//fmt.Println(numbers.IsPrime(19))
-	//
-	//fmt.Println(greeting.WelcomeText)
-	//
-	//fmt.Println(strings.Reverse("callicoder"))
-	//
-	//fmt.Println(str.Count("Go is Awesome. I love Go", "Go"))
+	fmt.Println(numbers.IsPrime(19))
+
+	fmt.Println(greeting.WelcomeText)
+
+	fmt.Println(strings.Reverse("callicoder"))
+
+	fmt.Println(str.Count("Go is Awesome. I love Go", "Go"))
 }
--- numbers/prime.go
+++ numbers/prime.go
@@ -6,7 +6,7 @@ import "math"
    - Ngày: 21.07.2021
    - By: Đức NĐ
 */
-func isPrime(num int) bool {
+func IsPrime(num int) bool {
 	for i := 2; i <= int(math.Floor(math.Sqrt(float64(num)))); i++ {
 		if num%i == 0 {
 			return false
